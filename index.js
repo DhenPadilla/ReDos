840115d1681f48a016c2db2b771e89e98f4016d5
Dhen E Padilla <dhenepadilla@gmail.com>
2020-03-30 15:47:12 +0100

    Initial ReDos for Ajv example

--- index.js
+++ index.js
@@ -1,10 +1,43 @@
 var express = require("express");
 var app = express();
 
+const utils = require("./utils").measureTime;
+
+var charset = require('charset');
+var Ajv = require('ajv');
+var ajv = new Ajv;
+
+
 app.listen(3000, () => {
  console.log("Server running on port 3000");
 });
 
-app.get("/url", (req, res, next) => {
-    res.json("Hello World!");
+app.post("/ajv/dos", (req, res, next) => {
+    var inp = format_ajv(req.body.title);
+    var time_taken = measureTime(ajv.compile(inp)); 
+    res.json(time_taken);
 })
+
+var format_ajv = function(str) {
+    return {
+        "type": "object",
+        "properties": {
+            "foo": {
+                "type": 'string',
+                "oneOf": [
+                    {"pattern": str}
+                ]
+            }
+        }
+    }
+}
+
+app.post("/ajv/mitigation", (req, res) => {
+
+})
+
+// app.get("/", (req, res, next) => {
+//     // req == genstr()
+//     final_req = //Format of ajv DoS {}
+//     res.json("Hello World!");
+// })
