4e277114879641f2d84bce851a3130c2195d0381
Weihang Huang <weihang.huang.16@ucl.ac.uk>
2020-03-31 20:40:48 +0100

    Add normal data

--- data/data_gen.py
+++ data/data_gen.py
@@ -3,7 +3,7 @@ import json
 def gen_string(pattern, number):
     return str(pattern) * number
 
-with open("markeddosdata.json", "w") as outfile:
-    value = gen_string('`', 8) + gen_string(' ', 700) + gen_string('`', 11)
+with open("markeddata.json", "w") as outfile:
+    value = gen_string('a', 719)
     data = {"title": value}
     json.dump(data, outfile)
